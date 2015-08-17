188d998628127e55e1c0bd31b40985b8ce9990b5
Sacha <Sacha>
2015-08-17 17:04:34 +0200

    Test User Presence

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-	def setup
-		@post = Post.new(title: "Title", content: "blabla", user_id:1)
-	end
-	test "should be valid" do
-		assert @post.valid?
+  def setup
+    @user = users(:michael)
+    @post = @user.posts.build(content: "Blabla")
+  end
+  test "should be valid" do
+    assert @post.valid?
+  end
+	test "user id should be present" do
+    @post.user_id = nil
+    assert_not @post.valid?
 	end
 end
