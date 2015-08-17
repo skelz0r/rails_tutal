0be602b7b8ef6aea6ab892680e3378d7a667223e
Sacha <Sacha>
2015-08-17 17:18:27 +0200

    fix some style

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
-	belongs_to :user
-	validates :user_id, presence: true
-	validates :content, presence:true
+  belongs_to :user
+  
+  validates :user_id, presence: true
+  validates :content, presence: true
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -5,9 +5,11 @@ class PostTest < ActiveSupport::TestCase
     @user = users(:michael)
     @post = @user.posts.build(content: "Blabla")
   end
+
   test "should be valid" do
     assert @post.valid?
   end
+  
 	test "user id should be present" do
     @post.user_id = nil
     assert_not @post.valid?
